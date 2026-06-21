e323be5dffa73ceafd04ca4425de5e0ce673d835
govindpvenu <govindpvenu.txt@gmail.com>
2026-06-21 22:40:03 +0530

    chore: setup neondb + drizzle orm

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div></div>;
+import { db } from "@/db/drizzle";
+
+async function getData() {
+  const response = await db.execute(`SELECT version()`);
+  return response;
+}
+export default async function Home() {
+  const data = await getData();
+  console.log("Database version:", data);
+  return <>asdf</>;
 }
